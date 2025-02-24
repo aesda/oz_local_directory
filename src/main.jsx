ed603bcd7caae1aae6d4f5f927c901b8fa53ac64
SANGITA RAI <165255817+aesda@users.noreply.github.com>
2025-02-24 19:42:16 +1100

    added tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
     <App />
+  </BrowserRouter>
   </StrictMode>,
 )
+
+
